4e84e516813c845b8b4a15bc872556883a9f27a2
Ku Ihsan <ihsannudin266@gmail.com>
2023-10-22 06:12:13 +0800

    Adding Effects

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour
 
     public int currentScore;
     public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
 
     public int currentMultiplier;
     public int multiplierTracker;
@@ -60,8 +62,26 @@ public class GameManager : MonoBehaviour
 
         multiText.text = "Multiplier: X" + currentMultiplier;
 
-        currentScore += scorePerNote * currentMultiplier;
+        //currentScore += scorePerNote * currentMultiplier;
         scoreText.text = "Score: " + currentScore;
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
     }
 
     public void NoteMissed()
--- Assets/Script/NoteObject.cs
+++ Assets/Script/NoteObject.cs
@@ -4,6 +4,7 @@ public class NoteObject : MonoBehaviour
 {
     public bool canBePressed;
     public KeyCode keyToPress;
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
     private bool obtained;
 
     // Start is called before the first frame update
@@ -20,7 +21,26 @@ public class NoteObject : MonoBehaviour
             {
                 gameObject.SetActive(false);
                 obtained = true;
-                GameManager.instance.NoteHit();
+                // GameManager.instance.NoteHit();
+
+                if (Mathf.Abs(transform.position.y) > 0.25)
+                {
+                    Debug.Log("Hit");
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
+                else if (Mathf.Abs(transform.position.y) > 0.05f)
+                {
+                    Debug.Log("Good");
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("Perfect");
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }
             }
         }
     }
@@ -43,6 +63,7 @@ public class NoteObject : MonoBehaviour
                 if (!obtained)
                 {
                     GameManager.instance.NoteMissed();
+                    Instantiate(missEffect, transform.position, missEffect.transform.rotation);
                 }
             }
             //GameManager.instance.NoteMissed();
