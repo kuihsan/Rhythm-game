a8862e85199982784c94ec2271b8638c5a22752b
Drystann <aimanzuhairi1402@gmail.com>
2024-01-06 18:55:51 +0800

    tambah la sikit

---
+++ Assets/Script/FootstepScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepScript : MonoBehaviour
+{
+    public AudioSource footstepsSound, sprintSound;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                footstepsSound.enabled = false;
+                sprintSound.enabled = true;
+            }
+            else
+            {
+                footstepsSound.enabled = true;
+                sprintSound.enabled = false;
+            }
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+            sprintSound.enabled = false;
+        }
+    }
+}
